b3abd34d1782eb67f929cc4f72fba8f6e97f96b1
richie <richievvvhuynh@gmail.com>
2020-09-16 16:28:59 -0700

    await and async refactor

--- seeds.js
+++ seeds.js
@@ -1,58 +1,108 @@
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
-var Comment = require("./models/comment");
+var Comment   = require("./models/comment");
 
-var data = [
+var seeds = [
     {
-         name: "Cloud's Rest", 
-         image: "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        name: "Cloud's Rest", 
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     },
     {
-         name: "Steins Gate", 
-         image: "https://onebigphoto.com/uploads/2013/01/heavenly-view-from-the-alps.jpg",
-         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-     },
-     {
-         name: "Half Dome", 
-         image: "https://obp-web-image-library.s3-us-west-2.amazonaws.com/Small+Group+Adventures/Seasons+of+Yosemite/Hero+Images/Seasons_halfDome.jpg",
-         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-     }
-]
+        name: "Desert Mesa", 
+        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+];
 
-function seedDB(){
-    //Remove all campgrounds
-    Campground.remove({}, function(err){
-        if(err){
-            console.log(err);
-        } else {
-            console.log("removed campgrounds!"); 
-            //add a few campgrounds
-        data.forEach(function(seed){
-            Campground.create(seed, function(err, campground){
-                if(err){
-                    console.log(err);
-                } else {
-                    console.log("added a campground!");
-                    //create a comment 
-                    Comment.create(
-                        {
-                            text: "This place is great, but I wish there was internet",
-                            author: "Lil Uzi Vert"
-                        }, function(err, comment){
-                            if(err){
-                                console.log(err);
-                            } else {
-                                campground.comments.push(comment);
-                                campground.save();
-                                console.log("Created new comment");
-                            }
-                        });
+async function seedDB(){
+    try {
+        await Campground.remove({});
+        console.log('Campgrounds removed');
+        await Comment.remove({});
+        console.log('Comments removed');
+
+        for(const seed of seeds) {
+            let campground = await Campground.create(seed);
+            console.log('Campground created');
+            let comment = await Comment.create(
+                {
+                    text: 'This place is great, but I wish there was internet',
+                    author: 'Homer'
                 }
-            })
-        });
-     }     
- });
-    //add a few comments
+            )
+            console.log('Comment created');
+            campground.comments.push(comment);
+            campground.save();
+            console.log('Comment added to campground');
+        }
+    } catch(err) {
+        console.log(err);
+    }
 }
+
 module.exports = seedDB;
+
+
+// var mongoose = require("mongoose");
+// var Campground = require("./models/campground");
+// var Comment = require("./models/comment");
+
+// var data = [
+//     {
+//          name: "Cloud's Rest", 
+//          image: "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350",
+//     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//     },
+//     {
+//          name: "Steins Gate", 
+//          image: "https://onebigphoto.com/uploads/2013/01/heavenly-view-from-the-alps.jpg",
+//          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//      },
+//      {
+//          name: "Half Dome", 
+//          image: "https://obp-web-image-library.s3-us-west-2.amazonaws.com/Small+Group+Adventures/Seasons+of+Yosemite/Hero+Images/Seasons_halfDome.jpg",
+//          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//      }
+// ]
+
+// function seedDB(){
+//     //Remove all campgrounds
+//     Campground.remove({}, function(err){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("removed campgrounds!"); 
+//             //add a few campgrounds
+//         data.forEach(function(seed){
+//             Campground.create(seed, function(err, campground){
+//                 if(err){
+//                     console.log(err);
+//                 } else {
+//                     console.log("added a campground!");
+//                     //create a comment 
+//                     Comment.create(
+//                         {
+//                             text: "This place is great, but I wish there was internet",
+//                             author: "Lil Uzi Vert"
+//                         }, function(err, comment){
+//                             if(err){
+//                                 console.log(err);
+//                             } else {
+//                                 campground.comments.push(comment);
+//                                 campground.save();
+//                                 console.log("Created new comment");
+//                             }
+//                         });
+//                 }
+//             })
+//         });
+//      }     
+//  });
+// }
+// module.exports = seedDB;
