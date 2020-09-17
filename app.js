3ef824cf1335e6a8955be06c4564f9b8ee51ef85
richie <richievvvhuynh@gmail.com>
2020-09-16 19:10:29 -0700

    Changed to bootstrap 4

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
+//require moment
+app.locals.moment = require('moment');
 // seedDB(); //seed the database
 
 
--- models/campground.js
+++ models/campground.js
@@ -1,26 +1,45 @@
-//schema setup
 var mongoose = require("mongoose");
+
+// SCHEME SETUP
 var campgroundSchema = new mongoose.Schema({
-    name: String,
-    price: String,
-    image: String,
-    description: String,
-    location: String,
-    lat: Number,
-    lng: Number,
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        },
-        username: String
+  name: String,
+  price: Number,
+  image: String,
+  imageId: String,
+  description: String,
+  location: String,
+  lat: Number,
+  lng: Number,
+  phone: String,
+  booking: {
+    start: String,
+    end: String
+  },
+  tags: [],
+  createdAt: { type: Date, default: Date.now },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
     },
-    comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Comment"
-        }
-    ]
+    username: String
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ],
+  rateAvg: Number,
+  rateCount: Number,
+  hasRated: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  ]
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
+
+
